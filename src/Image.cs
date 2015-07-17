0ed985affd7759439d164ff4d23dd59538a20439
Ogare <frost3000@hotmail.ru>
2015-07-17 15:04:28 +0300

    Added bitmap extensions. Image class is now separate file

---
+++ src/Image.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+
+namespace Mosaic
+{
+    internal class Image : IEquatable<Image>
+    {
+        public Image(String path, Color color, String hashcode)
+        {
+            this.path = path;
+            this.color = color;
+            this.hashcode = hashcode;
+        }
+        public bool Equals(Image other)
+        {
+            if (ReferenceEquals(null, other))
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            return other.path == this.path;
+        }
+        public override int GetHashCode()
+        {
+            return path.GetHashCode();
+        }
+        public String path;
+        public Color color;
+        public String hashcode;
+    }
+}
